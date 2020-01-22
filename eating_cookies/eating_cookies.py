09526ea41315208b36be7a2010969ef3696ac583
Sydney Crumley <srsimps19@gmail.com>
2020-01-22 15:03:57 -0600

    started on eating_cookies.py

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,7 +6,9 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+  if n <= 0:
+    return 0
+  
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
